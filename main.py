2bd0a8edc9b6e85c2c8ec33bdf838be3f1b20ce1
boevoyogurec <pvoit2006@gmail.com>
2024-12-20 21:57:36 +0300

    добавил названия песен

--- main.py
+++ main.py
@@ -27,25 +27,3 @@ playlist_b = {
 'Улетаю': 3.24,
 'Опять метель': 3.37,
 }
-
-def parse_playlist(playlist):
-    if isinstance(playlist, tuple):
-        return [tuple(song.split("; ")) for song in playlist]
-    elif isinstance(playlist, dict):
-        return [(title, time) for title, time in playlist.items()]
-    else:
-        raise ValueError("Unsupported playlist type")
-
-def get_duration(playlist, n):
-    songs = parse_playlist(playlist)
-    n = min(n, len(songs)) 
-    selected_songs = random.sample(songs, n)
-    total_duration = sum(float(time) for _, time in selected_songs)  
-    total_duration_timedelta = timedelta(minutes=int(total_duration), seconds=int((total_duration % 1) * 60)
-    return total_duration_timedelta
-
-total_time_c = get_duration(playlist_c, 3)
-total_time_b = get_duration(playlist_b, 3)
-
-print(f"Общее время звучания для playlist_c: {total_time_c}")
-print(f"Общее время звучания для playlist_b: {total_time_b}")
